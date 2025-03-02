13efec16a0a6173af2f5ec3d17628d56d1af5f6b
Matthew-Foley-Student <foleymat000@gmail.com>
2025-03-02 13:00:00 -0800

    Add files via upload

---
+++ Project Week3 - Copy/Project 2/Inventory.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Project_2
+{
+    partial class Inventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnHomepage = new Button();
+            lblResults = new Label();
+            btnDisplay = new Button();
+            selectFileDialog = new OpenFileDialog();
+            SuspendLayout();
+            // 
+            // btnHomepage
+            // 
+            btnHomepage.BackColor = SystemColors.ButtonShadow;
+            btnHomepage.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnHomepage.Location = new Point(709, 356);
+            btnHomepage.Name = "btnHomepage";
+            btnHomepage.Size = new Size(176, 92);
+            btnHomepage.TabIndex = 0;
+            btnHomepage.Text = "Back To Homepage";
+            btnHomepage.UseVisualStyleBackColor = false;
+            btnHomepage.Click += CloseWindow;
+            // 
+            // lblResults
+            // 
+            lblResults.AutoSize = true;
+            lblResults.Font = new Font("Lucida Console", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblResults.Location = new Point(2, 9);
+            lblResults.Name = "lblResults";
+            lblResults.Size = new Size(61, 13);
+            lblResults.TabIndex = 1;
+            lblResults.Text = "label1";
+            lblResults.Click += lblResults_Click;
+            // 
+            // btnDisplay
+            // 
+            btnDisplay.Font = new Font("Lucida Console", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnDisplay.Location = new Point(12, 356);
+            btnDisplay.Name = "btnDisplay";
+            btnDisplay.Size = new Size(175, 82);
+            btnDisplay.TabIndex = 2;
+            btnDisplay.Text = "Tempt Inventory Display";
+            btnDisplay.UseVisualStyleBackColor = true;
+            btnDisplay.Click += btnDisplay_Click;
+            // 
+            // selectFileDialog
+            // 
+            selectFileDialog.FileName = " SelectFileDialog";
+            // 
+            // Inventory
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(897, 463);
+            Controls.Add(btnDisplay);
+            Controls.Add(lblResults);
+            Controls.Add(btnHomepage);
+            Name = "Inventory";
+            Text = "Inventory";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnHomepage;
+        private Label lblResults;
+        private Button btnDisplay;
+        private OpenFileDialog selectFileDialog;
+    }
+}
---
+++ Project Week3 - Copy/Project 2/Inventory.cs
@@ -0,0 +1,90 @@
+﻿/*
+ * Matthew Foley
+ * CST-150
+ * Activity #1
+ * 2/20/25
+ * cictation
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_2
+{
+    public partial class Inventory : Form
+    {
+        public Inventory()
+        {
+            InitializeComponent();
+            selectFileDialog.InitialDirectory = Application.StartupPath + @"Data";
+            selectFileDialog.Title = "Browse txt Files";
+            selectFileDialog.DefaultExt = "txt";
+            selectFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            lblResults.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CloseWindow(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void lblResults_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            string txtFile = "";
+            string dirLocation = "";
+            const int PadSpace = 20;
+            string header1 = "Item", header2 = "Price", header3 = "Quantity", header4 = "Season", header5 = "Consumable";
+            string headerLine1 = "-----", headerLine2 = "-----", headerLine3 = "-----", headerLine4 = "-----", headerline5 = "-----";
+            txtFile = this.selectFileDialog.FileName;
+            if (this.selectFileDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                txtFile = this.selectFileDialog.FileName;
+                dirLocation = this.selectFileDialog.FileName;
+                string[] lines = File.ReadAllLines(txtFile);
+
+
+                lblResults.Text = "";
+                lblResults.Text = string.Format("{0}{1}{2}{3}{4}\n", header1.PadRight(PadSpace), header2.PadRight(PadSpace), header3.PadRight(PadSpace), header4.PadRight(PadSpace), header5.PadRight(PadSpace));
+                lblResults.Text += string.Format("{0}{1}{2}{3}{4}\n", headerLine1.PadRight(PadSpace), headerLine2.PadRight(PadSpace), headerLine3.PadRight(PadSpace), headerLine4.PadRight(PadSpace), headerline5.PadRight(PadSpace));
+                foreach (string line in lines)
+                {
+                    ///split each line into an array of elements
+                    string[] inventoryList = line.Split(", ");
+                    //Iterate through each element in the array
+                    //using a for loop instead of fareach loop
+                    for (int i = 0; i < inventoryList.Length; i++)
+                    {
+                        //Display each element using proper spacing
+                        lblResults.Text += inventoryList[i].PadRight(PadSpace);
+                    }
+                    lblResults.Text += "\n";
+                    //make the label visible
+
+                }
+                lblResults.Visible = true;
+            }
+        }
+    }
+}
